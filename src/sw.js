d9f3ff811642b968214b736ef108a1273c015294
marmooo <marmooo@users.noreply.github.com>
2022-11-26 13:01:17 +0900

    bump bootstrap from 5.2.2 to 5.2.3

--- src/sw.js
+++ src/sw.js
@@ -1,9 +1,9 @@
-var CACHE_NAME = "2022-11-12 09:40";
+var CACHE_NAME = "2022-11-26 12:58";
 var urlsToCache = [
   "/tts-demo/",
   "/tts-demo/index.js",
   "/tts-demo/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
